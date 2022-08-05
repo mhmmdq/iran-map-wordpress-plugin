6fe2a7f8f5aa664f8698211cf12279c5383e17b2
Mohammad Qasemi <mhmdqasemi81@gmail.com>
2022-08-06 04:18:33 +0430

    the cityFixed the problem of not limiting sellers in

--- includes/classes/Ajax.php
+++ includes/classes/Ajax.php
@@ -64,26 +64,38 @@ class Ajax {
     public function change_categories_to_sellers_page() {
         
 
-        if(isset($_REQUEST['term'])) {
+        if(isset($_REQUEST['term']) && isset($_REQUEST['city_id'])) {
             $term_id = (int) trim($_REQUEST['term']);
-            
+            $city_id = (int) trim($_REQUEST['city_id']);
             global $wpdb;
             $posts = $wpdb->get_results( "SELECT * FROM `{$wpdb->prefix}postmeta` WHERE `meta_key` = 'iran_map_product_category' AND `meta_value` = '{$term_id}'" );
             
+			$sellers_of_this_city = get_post_meta($city_id , 'iran_map_sellers' , 'true');
+			$sellers_of_this_city = str_replace("'" , '"' , $sellers_of_this_city);
+			$sellers_of_this_city = json_decode($sellers_of_this_city);
+
             $sellers = [];
             foreach($posts as $post) {
                 $post_id = $post->post_id;
                 $seller_id = (int) $wpdb->get_var( $wpdb->prepare( "SELECT post_author FROM {$wpdb->posts} WHERE ID = %d ", $post_id ) );
-    
-                $sellers[] = [
-                    'id' => $seller_id,
-                    'name' => dokan_get_vendor($seller_id)->get_shop_name(),
-                    'image' => get_user_meta($seller_id, 'iran_map_logo', true),
-                ];
+
+				if(in_array($seller_id , $sellers_of_this_city)) {
+					 $sellers[$seller_id] = [
+						'id' => $seller_id,
+						'name' => dokan_get_vendor($seller_id)->get_shop_name(),
+						'image' => get_user_meta($seller_id, 'iran_map_logo', true),
+                	];
+				}
+               
             }
             
-            $sellers = array_unique($sellers);
-            echo json_encode($sellers);
+			$sellers_unique = [];
+			foreach($sellers as $seller) {
+				$sellers_unique[] = $seller;
+			}
+            
+            echo json_encode($sellers_unique);
+
         }
         exit;
 
